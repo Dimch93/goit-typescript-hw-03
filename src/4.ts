9ecf091b8c2731851b584ae460ad7b91fabc26e1
Dimch93 <dimch93@ukr.net>
2023-12-31 14:59:30 +0200

    home3-1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Door open");
+    } else {
+      console.log("Door closed");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door open");
+    } else {
+      console.log("Door closed");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
